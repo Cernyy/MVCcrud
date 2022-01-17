1e3ec7556fae05e39ec9c5f8b708912dcf98c448
MartinCerny <Martincerny@gmx.at>
2022-01-17 11:40:45 +0100

    Projektdateien hinzufügen.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Display(Name ="Display Order")]
+        public int DisplayOrder { get; set; }
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+    }
+}
